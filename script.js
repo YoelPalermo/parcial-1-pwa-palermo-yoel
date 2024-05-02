53e9a6307d7eaa6c655f327a9296abd54e058390
YoelPalermo <yoel.palermo@davinci.edu.ar>
2024-05-02 17:10:10 -0300

    cards

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ const api_url = "https://pokeapi.co/api/v2/pokemon/";
 const numPokemons = 100;
 
 const promises = [];
-
+const searchInput = document.getElementById('searchInput');
+const searchBtn = document.getElementById('searchBtn');
 const resultsDiv = document.getElementById('results');
 const ul = document.createElement('ul');
 
@@ -40,6 +41,118 @@ async function fetchPokemons() {
 
             ul.appendChild(li);
 
+
+////////////card
+
+function cardPoke() {
+
+    descPokeDiv = document.createElement('div');
+    descPokeDiv.setAttribute('id', 'descPokeDiv');
+
+    descPoke = document.createElement('div');
+    descPoke.setAttribute('id', 'desc');
+
+    pokeContainer = document.getElementById('pokeContainer');
+
+    descPokeDiv.appendChild(descPoke);
+    pokeContainer.appendChild(descPokeDiv);
+
+    descPoke.setAttribute('class', 'bg-warning text-primary rounded p-4 text-center  text-capitalize m-auto  mt-3 border border-3 border-primary rounded w-70');
+    descPoke.innerHTML = `<div class="bg-primary text-warning p-4 pt-1 pb-2 rounded">
+<h1>
+${pokemon.id} - ${pokemon.name}
+</h1>
+</div>`;
+
+    const Sprite = document.createElement('img');
+    Sprite.setAttribute('class', ' sprite');
+    Sprite.src = pokemon.sprites.front_default;
+    descPoke.appendChild(Sprite);
+
+    const typeTitle = document.createElement('h2');
+    typeTitle.textContent = 'Type:';
+
+    typeTitle.setAttribute('class', 'bg-dark text-warning p-3 pt-0 rounded-top text-danger m-0');
+
+    descPoke.appendChild(typeTitle);
+
+    const ulTypes = document.createElement('ul');
+    ulTypes.setAttribute('class', 'bg-light rounded-bottom list-unstyled pb-2');
+
+    /*/////////////////////////////////////////////*/
+    pokemon.types.forEach(typeInfo => {
+        const li = document.createElement("li");
+        li.setAttribute('class', 'text-dark');
+        li.textContent = typeInfo.type.name;
+        ulTypes.appendChild(li);
+        descPoke.appendChild(ulTypes);
+
+    });
+
+
+
+    const abilitiesTytle = document.createElement('h2');
+    abilitiesTytle.textContent = 'Abilities:';
+    abilitiesTytle.setAttribute('class', 'bg-dark text-warning p-3 pt-0 rounded-top text-danger m-0');
+    descPoke.appendChild(abilitiesTytle);
+
+
+    ulAbilities = document.createElement('ul');
+    ulAbilities.setAttribute('class', 'bg-light rounded-bottom list-unstyled pb-2');
+
+
+    pokemon.abilities.forEach(typeInfo => {
+        const liAbility = document.createElement('li');
+        liAbility.setAttribute('class', 'text-dark');
+        liAbility.textContent = typeInfo.ability.name;
+
+        ulAbilities.appendChild(liAbility);
+
+        descPoke.appendChild(ulAbilities);
+
+    });
+
+
+    descPoke.innerHTML += `
+<div >
+<ul class="list-unstyled  text-light rounded d-flex">
+<li class="p-2 m-1 w-50 bg-primary rounded text-light">Height: ${pokemon.height}</li>
+<li class="p-2 m-1  w-50 bg-primary rounded  text-light">Weight: ${pokemon.weight}</li>
+</ul>
+</div>`;
+
+
+    /*/////////////////////////*/
+
+    closeButton = document.createElement('button');
+    closeButton.textContent = 'X';
+    closeButton.setAttribute('class', 'btn closeButton bg-danger text-center border border-0');
+
+
+    descPoke.appendChild(closeButton);
+
+
+
+    closeButton.addEventListener('click', () => {
+
+        if (descPokeDiv) {
+            descPokeDiv.remove();
+        }
+    })
+
+
+};
+
+////////////card
+
+
+
+
+            li.addEventListener('click', () => {
+             
+               
+                cardPoke();
+            });
             resultsDiv.appendChild(ul);
         });
 
